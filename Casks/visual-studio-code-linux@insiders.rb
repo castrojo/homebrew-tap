86210eb13489b9449e8e88bdd990cef525df37e1
Jorge O. Castro <jorge.castro@gmail.com>
2026-04-22 15:35:05 +0000

    visual-studio-code-linux@insiders 1.118.0-insider

--- Casks/visual-studio-code-linux@insiders.rb
+++ Casks/visual-studio-code-linux@insiders.rb
@@ -2,9 +2,9 @@ cask "visual-studio-code-linux@insiders" do
   arch arm: "arm64", intel: "x64"
   os linux: "linux"
 
-  version "1.116.0-insider"
-  sha256 arm64_linux:  "1e53507fd94fd3416d4ce16486669bc9580063833f3285f46e84a6cfb35df1d6",
-         x86_64_linux: "ca96532594217d5c8753ff9eadb605ac53e222662faea973be81fb612c724120"
+  version "1.118.0-insider"
+  sha256 arm64_linux:  "85f201e59cf09242e9e384db7a0150ba96d565d032aae940367ed97996275978",
+         x86_64_linux: "a6b22d0a6896952b74eb99c39bdfda4f496b49ee8a4a51b2d6bef8640d306426"
 
   url "https://update.code.visualstudio.com/#{version}/#{os}-#{arch}/insider"
   name "Microsoft Visual Studio Code Insiders"
